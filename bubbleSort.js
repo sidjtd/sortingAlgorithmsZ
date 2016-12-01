18fda3e3eae74bfff93bb10586b609f96ccee5e5
Sidney Dodson <siddodson@gmail.com>
2016-11-30 17:52:31 -1000

    Reversing direction literally one symbol change, no further edits needed

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,4 +1,4 @@
-function bubbleSortShrink(array){
+function bubbleSort(array){
   var yourLongness = array.length;
   var completed = false;
 
@@ -6,7 +6,7 @@ function bubbleSortShrink(array){
     completed = true;
 
     for(var ii = 0; ii < yourLongness; ii++){
-      if(array[ii-1] < array[ii]){
+      if(array[ii-1] > array[ii]){
         var holder = array[ii - 1];
         array[ii -1] = array[ii];
         array[ii] = holder;
@@ -18,6 +18,6 @@ function bubbleSortShrink(array){
 }
 
 
-bubbleSortShrink([7,5,2,4,3,9]); //[2, 3, 4, 5, 7, 9]
-bubbleSortShrink([9,7,5,4,3,1]); //[1, 3, 4, 5, 7, 9]
-bubbleSortShrink([1,2,3,4,5,6]); //[1, 2, 3, 4, 5, 6]
+bubbleSort([7,5,2,4,3,9]); //[2, 3, 4, 5, 7, 9]
+bubbleSort([9,7,5,4,3,1]); //[1, 3, 4, 5, 7, 9]
+bubbleSort([1,2,3,4,5,6]); //[1, 2, 3, 4, 5, 6]
